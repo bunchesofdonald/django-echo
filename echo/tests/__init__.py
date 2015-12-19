debca43097fe2a1a00835bd701056d03899e5ef3
Chris Pickett <chris.pickett@gmail.com>
2015-12-19 09:35:29 -0800

    Refactor tests into base class

---
+++ echo/tests/__init__.py
@@ -0,0 +1,75 @@
+import json
+
+from django.test import TestCase, RequestFactory
+
+
+class BaseEchoTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+        self.base_request_data = {
+            "session": {
+                "sessionId": "SessionId.123",
+                "application": {
+                    "applicationId": "amzn1.echo-sdk-ams.app.123"
+                },
+                "attributes": None,
+                "user": {
+                    "userId": "amzn1.echo-sdk-account.123"
+                },
+                "new": False
+            },
+            "request": None
+        }
+
+        self.launch_request = {
+            "type": "LaunchRequest",
+            "requestId": "EdwRequestId.123",
+            "timestamp": 1450024241777,
+        }
+
+        self.session_ended_request = {
+            "type": "SessionEndedRequest",
+            "requestId": "EdwRequestId.123",
+            "timestamp": 1450024241777,
+            "reason": "USER_INITIATED"
+        }
+
+        self.intent_request = {
+            "type": "IntentRequest",
+            "requestId": "EdwRequestId.123",
+            "timestamp": 1450024241777,
+            "intent": {
+                "name": "TheIntent",
+                "slots": {}
+            }
+        }
+
+    def _generate_request(self, request_data):
+        http_request = self.factory.post('/')
+        http_request._body = json.dumps(request_data)
+        http_request._body = json.dumps(request_data)
+        return http_request
+
+    def _generate_launch_request(self):
+        request_data = self.base_request_data
+        request_data['request'] = self.launch_request
+        return self._generate_request(request_data)
+
+    def _generate_session_ended_request(self):
+        request_data = self.base_request_data
+        request_data['request'] = self.session_ended_request
+        return self._generate_request(request_data)
+
+    def _generate_intent_request(self, intent_name="TheIntent", slots=None, session=None):
+        request_data = self.base_request_data
+        request_data['request'] = self.intent_request
+        request_data['request']['intent']['name'] = intent_name
+
+        if slots:
+            request_data['request']['intent']['slots'] = slots
+
+        if session:
+            request_data['session']['attributes'] = session
+
+        return self._generate_request(request_data)
--- echo/tests/test_skill.py
+++ echo/tests/test_skill.py
@@ -1,82 +1,33 @@
-import json
-
 import mock
-
-from django.test import TestCase, RequestFactory
 
 from echo.skill import (
     LAUNCH_NOT_IMPLEMENTED_ERROR,
     SESSION_ENDED_NOT_IMPLEMENTED_ERROR,
     EchoSkill,
 )
+from echo.tests import BaseEchoTestCase
 
 
-class TestEchoSkill(TestCase):
+class TestEchoSkill(BaseEchoTestCase):
     def setUp(self):
+        super(TestEchoSkill, self).setUp()
         self.skill = EchoSkill()
-
-        self.factory = RequestFactory()
-        self.http_request = self.factory.post('/')
-
-        self.base_request_data = {
-            "session": {
-                "sessionId": "SessionId.123",
-                "application": {
-                    "applicationId": "amzn1.echo-sdk-ams.app.123"
-                },
-                "attributes": None,
-                "user": {
-                    "userId": "amzn1.echo-sdk-account.123"
-                },
-                "new": False
-            },
-            "request": None
-        }
-
-        self.launch_request = {
-            "type": "LaunchRequest",
-            "requestId": "EdwRequestId.123",
-            "timestamp": 1450024241777,
-        }
-
-        self.session_ended_request = {
-            "type": "SessionEndedRequest",
-            "requestId": "EdwRequestId.123",
-            "timestamp": 1450024241777,
-            "reason": "USER_INITIATED"
-        }
-
-        self.intent_request = {
-            "type": "IntentRequest",
-            "requestId": "EdwRequestId.123",
-            "timestamp": 1450024241777,
-            "intent": {
-                "name": "TheIntent",
-                "slots": {}
-            }
-        }
 
     def test_routes_to_launch(self):
         """A LaunchRequest should raise a NotImplementedError"""
         expected = (NotImplementedError, LAUNCH_NOT_IMPLEMENTED_ERROR)
 
-        request_data = self.base_request_data
-        request_data['request'] = self.launch_request
-        self.http_request._body = json.dumps(request_data)
-
+        http_request = self._generate_launch_request()
         with self.assertRaisesMessage(*expected):
-            self.skill.dispatch(self.http_request)
+            self.skill.dispatch(http_request)
 
     def test_routes_to_session_ended(self):
         """A SessionEndedRequest should raise a NotImplementedError"""
         expected = (NotImplementedError, SESSION_ENDED_NOT_IMPLEMENTED_ERROR)
 
-        request_data = self.base_request_data
-        request_data['request'] = self.session_ended_request
-        self.http_request._body = json.dumps(request_data)
-
+        http_request = self._generate_session_ended_request()
         with self.assertRaisesMessage(*expected):
-            self.skill.dispatch(self.http_request)
+            self.skill.dispatch(http_request)
 
     def test_get_intent_handler_name(self):
         """get_intent_handler_name should know how to un-camelcase a string"""
@@ -87,13 +38,11 @@ class TestEchoSkill(TestCase):
         """A IntentRequest should be routed to the proper intent handler"""
         expected = 1
 
-        request_data = self.base_request_data
-        request_data['request'] = self.intent_request
-        self.http_request._body = json.dumps(request_data)
+        http_request = self._generate_intent_request(intent_name="TheIntent")
 
         self.skill.the_intent = None
         with mock.patch.object(self.skill, 'the_intent') as mock_intent:
-            self.skill.dispatch(self.http_request)
+            self.skill.dispatch(http_request)
             assert mock_intent.call_count == expected
 
     def test_routes_to_intent_with_session_and_slot_data(self):
@@ -110,14 +59,13 @@ class TestEchoSkill(TestCase):
             }
         }
 
-        request_data = self.base_request_data
-        request_data['request'] = self.intent_request
-        request_data['request']['intent']['slots'] = expected['slots']
-        request_data['session']['attributes'] = expected['session']
-
-        self.http_request._body = json.dumps(request_data)
+        http_request = self._generate_intent_request(
+            intent_name="TheIntent",
+            slots=expected['slots'],
+            session=expected['session']
+        )
 
         self.skill.the_intent = None
         with mock.patch.object(self.skill, 'the_intent') as mock_intent:
-            self.skill.dispatch(self.http_request)
+            self.skill.dispatch(http_request)
             mock_intent.assert_called_once_with(**expected)
